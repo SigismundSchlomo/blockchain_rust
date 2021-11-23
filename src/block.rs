31d8b8a08329b9b2e3ce0ef8409439754302a198
Alex Kuznietsov <alex@kuznietsov.dev>
2021-11-23 21:51:42 +0200

    Some starter code added

--- src/block.rs
+++ src/block.rs
@@ -1,1 +1,14 @@
-use std::fmt::{}
+use std::fmt::{ self, Debug, Formatter };
+
+pub struct Block {
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block")
+    }
+}
+
+impl Block {
+
+}
