f076cf474960d8b2b09c9bb2964a4fe20fc9a324
Alex Kuznietsov <alex@kuznietsov.dev>
2021-11-25 19:51:35 +0200

    Second part

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use blockchainlib::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut block = Block::new(0,0,vec![0; 32],0,"0".to_string());
+
+    println!("{:?}", &block );
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }
